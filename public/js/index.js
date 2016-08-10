0f8c631ff2923f98d6dcb5b20398c1e975b08678
kaosbeat <kasper.jordaens@gmail.com>
2016-08-10 11:20:58 +0200

    it works

--- public/js/index.js
+++ public/js/index.js
@@ -6,7 +6,7 @@ $(function() {
 
 // SOCKET.IO
 function initSockets(){
-    socket = io.connect('http://localhost:3000');
+    socket = io.connect('http://10.100.13.17:3000');
     
     socket.on('hello', function(data) {
         console.log("Socket hello:");
@@ -17,19 +17,22 @@ function initSockets(){
             }
         }
     });
+    var YPOS = 0;
     socket.on('iosono', function(data) {
     //    console.log("Socket iosono:");
-        
-    if (data.channel == 4) {
+    console.log(data);
+
+    if (data.channel == 4 & data.position.y != YPOS) {
 
         console.log(data.position.x, data.position.y, data.position.z );
-        setPos(data.position.x-5, data.position.y, data.position.z - 10);
+        setPos(data.position.x, data.position.y, data.position.z - 10);
     }
     });
 
 }
 
 ///VR shizzle
+var clap = document.querySelector('[sound]');
 var box = document.querySelector('a-box');
 function setPos (x,y,z) {
   box.setAttribute('position', {
@@ -37,6 +40,12 @@ function setPos (x,y,z) {
     y: y,
     z: z
   });
+  clap.setAttribute('position', {
+    x: x,
+    y, y,
+    z, z
+  });
+  clap.components.sound.play();
 }
 
 
